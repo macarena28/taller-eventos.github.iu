8a18df2f56bb0f9d8cd61799017b469a26278ac6
macarena28 <Macarenaanza000@gmail.com>
2024-08-19 19:21:41 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,6 @@
+<body>
+    <div id="container" style="padding: 5px; background-color: green;">
+        <button id="saludar-button">Saludar</button>
+    </div>
+    <script src="eventos.js"></script>
+</body>
